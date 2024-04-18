053f3b77f9b4572739f030d823d5dadb6632f3d3
Verified ✔️ <ryesan58@gmail.com>
2024-04-18 13:11:36 -0400

    adding json to make sure it can return json

--- ShortList/app.py
+++ ShortList/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 
 app = Flask(__name__)
@@ -6,7 +6,21 @@ app = Flask(__name__)
 @app.route('/')
 
 def index():
+    
     return 'Hello, World!'
+
+@app.route('/sample_json')
+def sample_json():
+    data = {
+        "users": [
+            {"id": 1, "name": "John Doe", "email": "john@example.com"},
+            {"id": 2, "name": "Jane Smith", "email": "jane@example.com"},
+            {"id": 3, "name": "Emily Jones", "email": "emily@example.com"}
+        ],
+        "success": True,
+        "message": "User data fetched successfully"
+    }
+    return jsonify(data)
 
 if __name__ == '__main__':
     app.run(debug=True)

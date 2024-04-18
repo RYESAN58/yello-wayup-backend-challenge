3a12f36d7b24a044ddf6df146592df67cdc38747
Verified ✔️ <ryesan58@gmail.com>
2024-04-18 14:46:53 -0400

    added the encoder so it takes a url and encodes it

--- ShortList/app.py
+++ ShortList/app.py
@@ -1,12 +1,19 @@
-from flask import Flask, jsonify
-
+from urllib import request
+from flask import Flask, jsonify, request
+import re
+import hashlib
+import base64
 
 app = Flask(__name__)
+
+
+
+url_mapping = {}
+
 
 @app.route('/')
 
 def index():
-    
     return 'Hello, World!'
 
 @app.route('/sample_json')
@@ -20,6 +27,23 @@ def sample_json():
         "success": True,
         "message": "User data fetched successfully"
     }
+    return jsonify(data)
+
+def encodeUrl(url):
+    pattern = re.compile(r'^(https?|ftp):\/\/[^\s/$.?#].[^\s]*$')
+    if (re.match(pattern, url)):
+        url_hash = hashlib.sha256(url.encode('utf-8')).digest()
+        short_id = base64.urlsafe_b64encode(url_hash).decode('utf-8')[:8]
+        url_mapping[short_id] = url
+
+        return short_id + ".co"
+    return url
+
+@app.route('/encode' , methods = ['POST'])
+def encode():
+    data = request.get_json()
+    print(data)
+    data['encoded'] = encodeUrl(data['url'])
     return jsonify(data)
 
 if __name__ == '__main__':
